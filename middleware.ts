830a347f245b281604deb6c0248992d4a8155ad3
gizems-ai <gizemburtecin@gmail.com>
2025-12-19 22:51:38 +0300

    fix: total reset of root and middleware

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,17 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(['/login(.*)', '/register(.*)']);
 
-export default clerkMiddleware((auth, request) => {
+export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    auth().protect();
+    await auth.protect();
   }
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Next.js iç dosyalarını ve statik dosyaları (resim vb.) atla
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Her zaman API rotalarını çalıştır
+    '/(api|trpc)(.*)',
+  ],
 };
